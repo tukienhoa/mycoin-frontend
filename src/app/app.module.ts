679cf9e51d00a04a55777a12972a94e1c752e311
tukienhoa <tukienhoa1710@gmail.com>
2022-06-14 22:28:22 +0700

    Visualize blocks on the chain and transactions of block

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
+import { BlockViewComponent } from './components/block-view/block-view.component';
+import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BlockchainViewerComponent,
+    BlockViewComponent,
+    TransactionsTableComponent
   ],
   imports: [
     BrowserModule,
