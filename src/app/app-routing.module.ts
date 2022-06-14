1f5f53f63e64222da2da28df4b6830af0aca40ad
tukienhoa <tukienhoa1710@gmail.com>
2022-06-14 23:13:31 +0700

    Add settings page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
+import { SettingsComponent } from './pages/settings/settings.component';
 
 const routes: Routes = [
-  { path: '', component: BlockchainViewerComponent}
+  { path: '', component: BlockchainViewerComponent},
+  { path: 'settings', component: SettingsComponent}
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,20 @@ import { AppComponent } from './app.component';
 import { BlockchainViewerComponent } from './pages/blockchain-viewer/blockchain-viewer.component';
 import { BlockViewComponent } from './components/block-view/block-view.component';
 import { TransactionsTableComponent } from './components/transactions-table/transactions-table.component';
+import { SettingsComponent } from './pages/settings/settings.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent,
     BlockchainViewerComponent,
     BlockViewComponent,
-    TransactionsTableComponent
+    TransactionsTableComponent,
+    SettingsComponent
   ],
   imports: [
     BrowserModule,
+    FormsModule,
     AppRoutingModule
   ],
   providers: [],
